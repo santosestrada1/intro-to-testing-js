7cd0f7ddcc8f4e990fa2d05990d9f4bf8b72b739
Santos Estrada <santos.estrada1@gmail.com>
2020-06-24 10:27:51 -0500

    Added Alex and Pat

--- code.js
+++ code.js
@@ -3,6 +3,15 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "";
+function sayHello(input) {
+    if (input === "Alex") {
+        return "Hello, Alex!";
+    }
+else if (input === "Pat"){
+        return "Hello, Pat!";
+    }
+else
+    {
+        return "Hello, Jane!";
+    }
 }
